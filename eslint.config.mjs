35cfa7cf15be1d608c97c025cd6554a1f335ff22
Rijenth <a_rijenth@laposte.net>
2024-07-03 00:06:28 +0200

    refacto: store eslint config into variables

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -20,6 +20,109 @@ const compat = new FlatCompat({
     allConfig: js.configs.all
 });
 
+const baseConfig = {
+    files: ["**/*.js", "**/*.jsx", "**/*.ts", "**/*.tsx"],
+
+    plugins: {
+        react: fixupPluginRules(react),
+        "react-hooks": fixupPluginRules(reactHooks),
+        prettier: fixupPluginRules(prettier),
+        "@typescript-eslint": fixupPluginRules(typescript),
+    },
+
+    ignores: ['dist', 'node_modules'],
+
+    languageOptions: {
+        globals: {
+            ...globals.browser,
+            ...globals.node,
+        },
+
+        ecmaVersion: 12,
+        sourceType: "module",
+
+        parserOptions: {
+            ecmaFeatures: {
+                jsx: true,
+            },
+        },
+    },
+
+    settings: {
+        react: {
+            version: "detect",
+        },
+    },
+
+    rules: {
+        // Ajouter ici les règles communes à tous les projets 
+        'consistent-return': 'error',
+
+        'import/prefer-default-export': 'off',
+
+        'no-console': 'off',
+        'no-underscore-dangle': 'off',
+        'no-restricted-syntax': 'off',
+        'no-plusplus': 'off',
+        'no-unused-expressions': 'warn',
+        
+        'prettier/prettier': 'error',
+
+        "react/react-in-jsx-scope": "off",
+
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        '@typescript-eslint/no-explicit-any': 'warn',
+        '@typescript-eslint/no-unused-vars': 'warn',
+        '@typescript-eslint/ban-ts-comment': 'warn',
+    },
+}
+
+const reactConfig = {
+    files: ["packages/react/**/*.js", "packages/react/**/*.jsx", "packages/react/**/*.ts", "packages/react/**/*.tsx"],
+
+    plugins: {
+        react: fixupPluginRules(react),
+        "react-hooks": fixupPluginRules(reactHooks),
+    },
+
+    rules: {
+        'react/jsx-props-no-spreading': 'off',
+        'react-refresh/only-export-components': 'off',
+        'react-hooks/rules-of-hooks': 'error',
+        'react-hooks/exhaustive-deps': 'off',
+        'react/require-default-props': 'off',
+        'react/no-array-index-key': 'off',
+        'react/jsx-props-no-spreading': 'off',
+    },
+    settings: {
+        'import/resolver': {
+            node: {
+                paths: ['./src'],
+                extensions: ['.js', '.jsx', '.ts', '.tsx', '.d.ts', '.css'],
+            },
+            typescript: {
+                project: './tsconfig.json',
+            },
+            alias: {
+                map: [['~', path.resolve(__dirname, './src')]],
+                extensions: ['.js', '.jsx', '.ts', '.tsx', '.d.ts'],
+            },
+        },
+    }
+}
+
+const reactNativeConfig = {
+    files: ["packages/react_native/**/*.js", "packages/react_native/**/*.jsx", "packages/react_native/**/*.ts", "packages/react_native/**/*.tsx"],
+    plugins: {
+        "react-native": reactNative,
+    },
+    rules: {
+        "react-native/no-inline-styles": "off",
+        "react-native/no-raw-text": "off",
+    },
+}
+
 export default defineConfig([...fixupConfigRules(compat.extends(
         "eslint:recommended",
         "plugin:react/recommended",
@@ -27,103 +130,7 @@ export default defineConfig([...fixupConfigRules(compat.extends(
         "plugin:prettier/recommended",
         "plugin:@typescript-eslint/recommended",
     )), 
-    {
-        plugins: {
-            react: fixupPluginRules(react),
-            "react-hooks": fixupPluginRules(reactHooks),
-            prettier: fixupPluginRules(prettier),
-            "@typescript-eslint": fixupPluginRules(typescript),
-        },
-
-        ignores: ['dist', 'node_modules'],
-
-        languageOptions: {
-            globals: {
-                ...globals.browser,
-                ...globals.node,
-            },
-
-            ecmaVersion: 12,
-            sourceType: "module",
-
-            parserOptions: {
-                ecmaFeatures: {
-                    jsx: true,
-                },
-            },
-        },
-
-        settings: {
-            react: {
-                version: "detect",
-            },
-        },
-    },
-    {
-        files: ["**/*.js", "**/*.jsx", "**/*.ts", "**/*.tsx"],
-        rules: {
-            // Ajouter ici les règles communes à tous les projets 
-            'consistent-return': 'error',
-
-            'import/prefer-default-export': 'off',
-
-            'no-console': 'off',
-            'no-underscore-dangle': 'off',
-            'no-restricted-syntax': 'off',
-            'no-plusplus': 'off',
-            'no-unused-expressions': 'warn',
-            
-            'prettier/prettier': 'error',
-
-            "react/react-in-jsx-scope": "off",
-
-            '@typescript-eslint/explicit-function-return-type': 'off',
-            '@typescript-eslint/explicit-module-boundary-types': 'off',
-            '@typescript-eslint/no-explicit-any': 'warn',
-            '@typescript-eslint/no-unused-vars': 'warn',
-            '@typescript-eslint/ban-ts-comment': 'warn',
-        },
-    },
-    {
-        files: ["packages/react/**/*.js", "packages/react/**/*.jsx", "packages/react/**/*.ts", "packages/react/**/*.tsx"],
-        plugins: {
-            react: fixupPluginRules(react),
-            "react-hooks": fixupPluginRules(reactHooks),
-        },
-
-        rules: {
-            'react/jsx-props-no-spreading': 'off',
-            'react-refresh/only-export-components': 'off',
-            'react-hooks/rules-of-hooks': 'error',
-            'react-hooks/exhaustive-deps': 'off',
-            'react/require-default-props': 'off',
-            'react/no-array-index-key': 'off',
-            'react/jsx-props-no-spreading': 'off',
-        },
-        settings: {
-            'import/resolver': {
-                node: {
-                    paths: ['./src'],
-                    extensions: ['.js', '.jsx', '.ts', '.tsx', '.d.ts', '.css'],
-                },
-                typescript: {
-                    project: './tsconfig.json',
-                },
-                alias: {
-                    map: [['~', path.resolve(__dirname, './src')]],
-                    extensions: ['.js', '.jsx', '.ts', '.tsx', '.d.ts'],
-                },
-            },
-        }
-    },
-    {
-        files: ["packages/react_native/**/*.js", "packages/react_native/**/*.jsx", "packages/react_native/**/*.ts", "packages/react_native/**/*.tsx"],
-        plugins: {
-            "react-native": reactNative,
-        },
-        rules: {
-            "react-native/no-inline-styles": "off",
-            "react-native/no-raw-text": "off",
-        },
-    },
+    baseConfig,
+    reactConfig,
+    reactNativeConfig,
 ]);
