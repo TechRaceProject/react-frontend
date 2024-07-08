1a9ceb746ba54019ed7cb53d1d9775e365df7ba0
Hassan Aidibe <101555873+HascoVice@users.noreply.github.com>
2024-07-08 17:20:25 +0200

    ajout de websocket pour envoyer du react native directement a la voiture et debut de structure de la voiture

---
+++ packages/react_native/socket.config.ts
@@ -0,0 +1,3 @@
+const socket = new WebSocket('ws://192.168.11.50/ws');
+
+export default socket;
---
+++ packages/react_native/src/enums/command.ts
@@ -0,0 +1,11 @@
+enum CommandCar {
+    WheelSpeed = 1,
+    VehiculeFace,
+    HeadRotation,
+    LedAnimation,
+    LedPrincipalColor,
+    LedSecondaryColor,
+    BuzzerAlarm,
+    BuzzerAlarmFrequency,
+    VideoCaptor
+}
