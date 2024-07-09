056e300ab498725443bfde7c7806f7e5542e0265
Hassan Aidibe <101555873+HascoVice@users.noreply.github.com>
2024-07-09 19:04:58 +0200

    Implement carCommand Screen , refactorisation carCommand Enum

--- packages/react_native/App.tsx
+++ packages/react_native/App.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { SafeAreaView, StyleSheet, View } from 'react-native';
 
-import Joystick from './src/components/Joystick';
+import CarControlScreen from './src/screens/CarControlScreen';
 import {
     ErrorEvent,
     ExceptionEvent,
@@ -37,45 +37,17 @@ function App(): React.JSX.Element {
     console.log('joystickPosition', joystickPosition);
 
     return (
-        <SafeAreaView style={[styles.safeArea]}>
-            <View style={styles.appContainer}>
-                <Joystick
-                    dimension={{ width: 300, height: 300 }}
-                    size={100}
-                    styles={{ ...styles }}
-                    state={{ joystickPosition, setJoystickPosition }}
-                    uriJoystick={require('./src/assets/joystick.png')}
-                    uriJoystickArrow={require('./src/assets/joystick_arrow.png')}
-                />
-            </View>
+        <SafeAreaView style={styles.appContainer}>
+            <CarControlScreen/>
         </SafeAreaView>
     );
 };
 
 const styles = StyleSheet.create({
-    safeArea: {
-        flex: 1,
-    },
     appContainer: {
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-    },
-    sectionContainer: {
-        marginTop: 32,
-        paddingHorizontal: 24,
-    },
-    container: {
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    containerJoystick: {
-        position: 'relative',
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    joystick: {
-        position: 'absolute',
     },
 });
 
