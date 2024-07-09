8de11595616414d0bbb627d1dee8c78c3329ed05
Hikky <mabroukr1997@gmail.com>
2024-07-09 10:47:57 +0200

    update feature joystick

--- packages/react_native/App.tsx
+++ packages/react_native/App.tsx
@@ -5,59 +5,24 @@
  * @format
  */
 
-import React from 'react';
-import type { PropsWithChildren } from 'react';
-import {
-    SafeAreaView,
-    ScrollView,
-    StatusBar,
-    StyleSheet,
-    Text,
-    useColorScheme,
-    View,
-} from 'react-native';
+import React, { useState } from 'react';
+import { SafeAreaView, StyleSheet, View } from 'react-native';
 
-import {
-    Colors,
-    DebugInstructions,
-    Header,
-    LearnMoreLinks,
-    ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 import JoyStick from './src/components/Joystick.tsx';
 
-type SectionProps = PropsWithChildren<{
-    title: string;
-}>;
-
-function Section({ children, title }: SectionProps): React.JSX.Element {
-    const isDarkMode = useColorScheme() === 'dark';
-    return <View style={styles.sectionContainer}></View>;
-}
-
 function App(): React.JSX.Element {
-    const isDarkMode = useColorScheme() === 'dark';
-
-    const backgroundStyle = {
-        backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-        flex: 1,
-    };
+    const [joystickPosition, setJoystickPosition] = useState({ x: 0, y: 0 });
 
     return (
-        <SafeAreaView style={[backgroundStyle, styles.safeArea]}>
-            <StatusBar
-                barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-                backgroundColor={backgroundStyle.backgroundColor}
-            />
+        <SafeAreaView style={[styles.safeArea]}>
             <View style={styles.appContainer}>
-                <Text>Toto</Text>
-                <Text>Toto</Text>
                 <JoyStick
                     dimension={{ width: 300, height: 300 }}
-                    position={{ x: 150, y: 150 }}
                     size={100}
                     styles={{ ...styles }}
-                    uri={'../assets/joystick.svg'}
+                    state={{ joystickPosition, setJoystickPosition }}
+                    uriJoystick={require('./src/assets/joystick.png')}
+                    uriJoystickArrow={require('./src/assets/joystick_arrow.png')}
                 />
             </View>
         </SafeAreaView>
@@ -77,45 +42,18 @@ const styles = StyleSheet.create({
         marginTop: 32,
         paddingHorizontal: 24,
     },
-    sectionTitle: {
-        fontSize: 24,
-        fontWeight: '600',
-    },
-    sectionDescription: {
-        marginTop: 8,
-        fontSize: 18,
-        fontWeight: '400',
-    },
-    highlight: {
-        fontWeight: '700',
-    },
     container: {
-        width: 300,
-        height: 300,
+        flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        borderColor: 'red',
-        borderWidth: 5,
     },
     containerJoystick: {
-        width: '100%',
-        height: '100%',
-        borderColor: 'yellow',
-        borderWidth: 5,
+        position: 'relative',
         justifyContent: 'center',
         alignItems: 'center',
     },
     joystick: {
-        backgroundColor: 'blue',
-        borderColor: 'white',
-        borderWidth: 5,
         position: 'absolute',
-        width: 100,
-        height: 100,
-    },
-    joystickArrow: {
-        width: '100%',
-        height: '100%',
     },
 });
 
--- packages/react_native/src/components/Joystick.tsx
+++ packages/react_native/src/components/Joystick.tsx
@@ -1,34 +1,52 @@
-import React, { useState } from 'react';
+import React from 'react';
 import {
+    GestureResponderEvent,
+    Image,
     ImageBackground,
     PanResponder,
-    StyleSheet,
     View,
     ViewStyle,
 } from 'react-native';
 
 interface JoystickProps {
     dimension: { width: number; height: number };
-    position: { x: number; y: number };
     size: number;
+    state: {
+        joystickPosition: {
+            x: number;
+            y: number;
+        };
+        setJoystickPosition: (position: { x: number; y: number }) => void;
+    };
     styles: {
         container: ViewStyle;
         containerJoystick: ViewStyle;
         joystick: ViewStyle;
-        joystickArrow: ViewStyle;
     };
-    uri: string;
+    uriJoystick: string;
+    uriJoystickArrow: string;
 }
 
-function Joystick({ dimension, position, size, styles, uri }: JoystickProps) {
-    const [joystickPosition, setJoystickPosition] = useState({ x: (300 - size) / 2, y: (300 - size) / 2 });
-
-    const handlePanResponderMove = (event, gestureState) => {
+function Joystick({
+    dimension,
+    size,
+    styles,
+    state: { joystickPosition, setJoystickPosition },
+    uriJoystick,
+    uriJoystickArrow,
+}: JoystickProps): React.JSX.Element {
+    const handlePanResponderMove = (
+        event: GestureResponderEvent,
+        gestureState: { dx: number; dy: number }
+    ): void => {
         let newX = joystickPosition.x + gestureState.dx;
         let newY = joystickPosition.y + gestureState.dy;
 
-        newX = Math.max(0, Math.min(newX, 300 - size));
-        newY = Math.max(0, Math.min(newY, 300 - size));
+        const maxMoveX = dimension.width - size / 2;
+        const maxMoveY = dimension.height - size / 2;
+
+        newX = Math.max(-size / 2, Math.min(newX, maxMoveX));
+        newY = Math.max(-size / 2, Math.min(newY, maxMoveY));
 
         setJoystickPosition({ x: newX, y: newY });
     };
@@ -39,56 +57,46 @@ function Joystick({ dimension, position, size, styles, uri }: JoystickProps) {
         onPanResponderMove: handlePanResponderMove,
         onPanResponderRelease: () => {
             setJoystickPosition({
-                x: (300 - size) / 2,
-                y: (300 - size) / 2,
+                x: (dimension.width - size) / 2,
+                y: (dimension.height - size) / 2,
             });
         },
     });
 
     return (
         <View
-            style={[styles.container, { backgroundColor: 'black', width: dimension.width, height: dimension.height }]}
+            style={[
+                styles.container,
+                { width: dimension.width, height: dimension.height },
+            ]}
             {...panResponder.panHandlers}
         >
-            <View style={[styles.containerJoystick, { width: 300, height: 300 }]}>
+            <ImageBackground source={uriJoystickArrow}>
                 <View
                     style={[
-                        styles.joystick,
+                        styles.containerJoystick,
                         {
-                            left: joystickPosition.x,
-                            top: joystickPosition.y,
-                            height: size,
-                            width: size,
+                            width: dimension.width,
+                            height: dimension.height,
                         },
                     ]}
-                />
-            </View>
+                >
+                    <Image
+                        source={uriJoystick}
+                        style={[
+                            styles.joystick,
+                            {
+                                left: joystickPosition.x,
+                                top: joystickPosition.y,
+                                height: size,
+                                width: size,
+                            },
+                        ]}
+                    />
+                </View>
+            </ImageBackground>
         </View>
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    containerJoystick: {
-        position: 'relative',
-        backgroundColor: 'red',
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    joystick: {
-        position: 'absolute',
-        backgroundColor: 'blue',
-    },
-    joystickArrow: {
-        width: '100%',
-        height: '100%',
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-});
 
 export default Joystick;
