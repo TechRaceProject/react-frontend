a5aaf9801482703413eee66718ea15402a76297a
jubs_kan3ki <aitaddajuba93@gmail.com>
2024-07-16 22:47:48 +0200

    chore(mocking ): prepare for car data mocking until API is ready

---
+++ packages/react/src/api/vehicle/vehicle.api.tsx
@@ -0,0 +1,20 @@
+import { ApiReturn } from '~/interfaces/utils/api.interface';
+
+class ApiVehicle {
+    static async fetchData(): Promise<ApiReturn> {
+        try {
+            const response = await fetch('../../data/fake.data.json');
+            const data = await response.json();
+            return { data, error: null, isLoading: false };
+        } catch (error) {
+            console.error('Failed to fetch data', error);
+            return {
+                data: null,
+                error: 'Failed to fetch data',
+                isLoading: false,
+            };
+        }
+    }
+}
+
+export default ApiVehicle;
