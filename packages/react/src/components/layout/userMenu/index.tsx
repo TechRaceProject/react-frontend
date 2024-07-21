f7707e7e826e3bfef481f2da8560f12c4d3669d4
jubs_kan3ki <aitaddajuba93@gmail.com>
2024-07-21 20:18:35 +0200

    feat(userMenu): improve logic, design, and animations

--- packages/react/src/components/layout/userMenu/index.tsx
+++ packages/react/src/components/layout/userMenu/index.tsx
@@ -11,24 +11,28 @@ function UserMenu() {
         setIsOpen(!isOpen);
     };
 
+    const menuClass = isOpen
+        ? 'UserMenu UserMenu-open'
+        : 'UserMenu UserMenu-close';
+
     return (
         <>
             <img
-                src="https://static1.squarespace.com/static/656f4e4dababbd7c042c4946/657236350931ee4538eea52c/65baf15103d8ad2826032a8a/1707422532886/how-to-stop-being-a-people-pleaser-1_1.jpg?format=1500w"
+                src={user.pp}
                 className="ImgProfile"
                 alt="user photo"
                 onClick={handleToggle}
             />
             {isOpen && (
-                <div className="UserMenu">
-                    <div>
-                        <p>{user.username}</p>
-                        <p>{user.email}</p>
-                    </div>
-                    <div>
-                        <p onClick={ApiAuth.logout}>Quitter</p>
-                    </div>
+            <div className={menuClass}>
+                <div className="UserMenu-top">
+                    <h4>{user.username}</h4>
+                    <p>{user.email}</p>
                 </div>
+                <div className="UserMenu-bottom" onClick={ApiAuth.logout}>
+                    <p>Quitter</p>
+                </div>
+            </div>
             )}
         </>
     );
--- packages/react/src/store/slice/user.slice.ts
+++ packages/react/src/store/slice/user.slice.ts
@@ -2,15 +2,18 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 export interface UserState {
     id: number;
+    pp: string;
     email: string;
     username: string | undefined;
     created_at: string;
     updated_at: string;
 }
 
-export const initialState: UserState = {
+const initialState: UserState = {
     id: 0,
+    pp: 'https://static1.squarespace.com/static/656f4e4dababbd7c042c4946/657236350931ee4538eea52c/65baf15103d8ad2826032a8a/1707422532886/how-to-stop-being-a-people-pleaser-1_1.jpg?format=1500w',
     email: '',
+    username: '',
     username: '',
     created_at: '',
     updated_at: '',
@@ -22,6 +25,7 @@ const userSlice = createSlice({
     reducers: {
         setUserState: (state, action: PayloadAction<UserState>) => {
             state.id = action.payload.id;
+            state.pp = action.payload.pp;
             state.email = action.payload.email;
             state.username = action.payload.username || '';
             state.created_at = action.payload.created_at;
