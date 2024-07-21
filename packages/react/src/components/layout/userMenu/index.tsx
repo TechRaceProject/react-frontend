0533936713f0839b2f718ab4602f74ee1e534e75
jubs_kan3ki <aitaddajuba93@gmail.com>
2024-07-21 20:18:35 +0200

    feat(userMenu): improve logic, design, and animations

--- packages/react/src/components/layout/userMenu/index.tsx
+++ packages/react/src/components/layout/userMenu/index.tsx
@@ -1,33 +1,38 @@
 import { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { RootState } from '~/store/store';
 import ApiAuth from '~/api/auth/auth.api';
 import './style.css';
 
 function UserMenu() {
     const [isOpen, setIsOpen] = useState(false);
+    const user = useSelector((state: RootState) => state.user);
 
     const handleToggle = () => {
         setIsOpen(!isOpen);
     };
 
+    const menuClass = isOpen
+        ? 'UserMenu UserMenu-open'
+        : 'UserMenu UserMenu-close';
+
     return (
         <>
             <img
-                src="https://static1.squarespace.com/static/656f4e4dababbd7c042c4946/657236350931ee4538eea52c/65baf15103d8ad2826032a8a/1707422532886/how-to-stop-being-a-people-pleaser-1_1.jpg?format=1500w"
+                src={user.pp}
                 className="ImgProfile"
                 alt="user photo"
                 onClick={handleToggle}
             />
-            {isOpen && (
-                <div className="UserMenu">
-                    <div>
-                        <p>Neil Sims</p>
-                        <p>neil.sims@flowbite.com</p>
-                    </div>
-                    <div>
-                        <p onClick={ApiAuth.logout}>Sign out</p>
-                    </div>
+            <div className={menuClass}>
+                <div className="UserMenu-top">
+                    <h4>{user.username}</h4>
+                    <p>{user.email}</p>
                 </div>
-            )}
+                <div className="UserMenu-bottom" onClick={ApiAuth.logout}>
+                    <p>Sign out</p>
+                </div>
+            </div>
         </>
     );
 }
--- packages/react/src/store/slice/user.slice.ts
+++ packages/react/src/store/slice/user.slice.ts
@@ -1,31 +1,37 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
-interface AuthState {
+interface UserState {
     id: number;
+    pp: string;
     email: string;
+    username: string;
     created_at: string;
     updated_at: string;
 }
 
-const initialState: AuthState = {
+const initialState: UserState = {
     id: 0,
+    pp: 'https://static1.squarespace.com/static/656f4e4dababbd7c042c4946/657236350931ee4538eea52c/65baf15103d8ad2826032a8a/1707422532886/how-to-stop-being-a-people-pleaser-1_1.jpg?format=1500w',
     email: '',
+    username: '',
     created_at: '',
     updated_at: '',
 };
 
-const authSlice = createSlice({
+const userSlice = createSlice({
     name: 'user',
     initialState,
     reducers: {
-        setUserState: (state, action: PayloadAction<AuthState>) => {
+        setUserState: (state, action: PayloadAction<UserState>) => {
             state.id = action.payload.id;
+            state.pp = action.payload.pp;
             state.email = action.payload.email;
+            state.username = action.payload.username;
             state.created_at = action.payload.created_at;
             state.updated_at = action.payload.updated_at;
         },
     },
 });
 
-export const { setUserState } = authSlice.actions;
-export default authSlice.reducer;
+export const { setUserState } = userSlice.actions;
+export default userSlice.reducer;
