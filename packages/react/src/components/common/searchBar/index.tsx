0e9a689869c8193ec3cae3ca9517e5c3b994640d
jubs_kan3ki <aitaddajuba93@gmail.com>
2024-08-21 12:41:07 +0200

    feat(history): create initial page with logic, design, and components

---
+++ packages/react/src/components/common/searchBar/index.tsx
@@ -0,0 +1,34 @@
+import { FaSearch } from 'react-icons/fa';
+import './style.css';
+
+interface SearchBarProps {
+    searchTerm: string;
+    onSearchChange: (term: string) => void;
+    placeholder?: string;
+    error?: string;
+}
+
+function SearchBar({
+    searchTerm,
+    onSearchChange,
+    placeholder = 'Search by race name',
+    error,
+}: SearchBarProps) {
+    return (
+        <div className="search-bar-container">
+            <div className="search-bar">
+                <FaSearch className="search-icon" />
+                <input
+                    type="text"
+                    placeholder={placeholder}
+                    value={searchTerm}
+                    onChange={(e) => onSearchChange(e.target.value)}
+                    className={`search-input ${error ? 'input-error' : ''}`}
+                />
+            </div>
+            {error && <small className="input-error-message">{error}</small>}
+        </div>
+    );
+}
+
+export default SearchBar;
