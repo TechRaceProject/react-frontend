fb90522d34e1631eb93a16e4991e60e39d02e8cf
Hassan Aidibe <101555873+HascoVice@users.noreply.github.com>
2024-09-12 04:23:43 +0200

    Implement differential power car

---
+++ packages/react_native/src/enums/CarDifferentialRatio/CarLowSpeedDifferentialRatioEnum.ts
@@ -0,0 +1,5 @@
+export enum CarLowSpeedDifferentialRatioEnum {
+    Low = 0,
+    Middle = 90,
+    High = 500,
+}
---
+++ packages/react_native/src/enums/CarDifferentialRatio/CarNormalSpeedDifferentialRatioEnum.ts
@@ -0,0 +1,5 @@
+export enum CarNormalSpeedDifferentialRatioEnum {
+    Low = 40,
+    Middle = 60,
+    High = 101,
+}
