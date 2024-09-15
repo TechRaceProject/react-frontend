de696e7f6b58f4a93bdc02d83f6f73012003e654
Hikky <mabroukr1997@gmail.com>
2024-09-15 20:48:11 +0200

    fix lint

--- packages/react_native/src/socket.config.ts
+++ packages/react_native/src/socket.config.ts
@@ -1,24 +1,19 @@
 const socket = new WebSocket('ws://192.168.0.50/ws');
 
-
 socket.onopen = () => {
-  console.log('Connexion WebSocket établie.');
+    console.log('Connexion WebSocket établie.');
 };
-
 
 socket.onmessage = (event) => {
-  console.log('Message reçu :', event.data);
+    console.log('Message reçu :', event.data);
 };
-
 
 socket.onclose = (event: Event) => {
-  console.log('Connexion fermée');
-
+    console.log('Connexion fermée');
 };
 
-
 socket.onerror = (error: Event) => {
-  console.error('Une erreur WebSocket est survenue :', error);
+    console.error('Une erreur WebSocket est survenue :', error);
 };
 
 export default socket;
