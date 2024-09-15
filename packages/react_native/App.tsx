d13a541b2331cf2e8df2c5123928dd0275c0ccd5
Hikky <mabroukr1997@gmail.com>
2024-09-15 20:44:07 +0200

    implement logic screen

--- packages/react_native/App.tsx
+++ packages/react_native/App.tsx
@@ -1,7 +1,11 @@
-import React from 'react';
-import { SafeAreaView, StyleSheet, View } from 'react-native';
-import TechRaceScreen from './src/screens/TechRaceScreen';
-import BottomNavigationBar from './src/components/BottomNavigationBar';
+import React, { useState, useEffect } from 'react';
+import { SafeAreaView, StyleSheet } from 'react-native';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 import {
     ErrorEvent,
     ExceptionEvent,
@@ -12,16 +16,27 @@ import {
 import useSSE from './src/hooks/useServerSentEvent';
 import { handleSSEMessage } from './src/utils/handleSSEMessage';
 
+import LoginRegisterScreen from './src/screens/LoginRegisterScreen';
+import TechRaceScreen from './src/screens/TechRaceScreen';
+import CarVideoControlScreen from './src/screens/CarVideoControlScreen';
+
+import { setHostUrl } from '../shared/index';
+
+const Stack = createNativeStackNavigator();
+
 function App(): React.JSX.Element {
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+
     /**
      * on utilise l'ip : 'http://10.0.2.2:8000' pour se connecter à l'api car
      * l'émulateur android ne peut pas se connecter à l'api en localhost
      */
-    const apiUrl = 'http://10.0.2.2:8000/api';
-    // @TODO : fix Url (Pas sure)
+    const apiUrl = '192.168.1.88:8000';
+
+    setHostUrl(apiUrl);
 
     useSSE(
-        apiUrl + '/sse',
+        'http://' + apiUrl + '/api/sse',
         (event: OpenEvent) => {
             console.log('Connexion SSE ouverte:', event);
         },
@@ -33,25 +48,63 @@ function App(): React.JSX.Element {
         }
     );
 
+    useEffect(() => {
+        const checkAuth = async () => {
+            try {
+                const token = await AsyncStorage.getItem('authToken');
+                if (token) {
+                    setIsAuthenticated(true);
+                } else {
+                    setIsAuthenticated(false);
+                }
+            } catch (e) {
+                console.error(e);
+                setIsAuthenticated(false);
+            }
+        };
+
+        checkAuth();
+    }, []);
+
     return (
-        <SafeAreaView style={styles.container}>
-            <TechRaceScreen />
-            <View style={styles.bottomBar}>
-                <BottomNavigationBar />
-            </View>
+        <SafeAreaView style={[styles.appContainer]}>
+            <NavigationContainer>
+                <Stack.Navigator initialRouteName={isAuthenticated ? 'TechRace' : 'LoginRegister'}>
+                    {!isAuthenticated ? (
+                        <Stack.Screen
+                            name="LoginRegister"
+                            component={LoginRegisterScreen}
+                            options={{
+                                headerShown: false,
+                            }}
+                        />
+                    ) : (
+                        <>
+                            <Stack.Screen
+                                name="TechRace"
+                                component={TechRaceScreen}
+                                options={{
+                                    headerShown: false,
+                                }}
+                            />
+                            <Stack.Screen
+                                name="CarVideoControl"
+                                component={CarVideoControlScreen}
+                                options={{
+                                    headerShown: false,
+                                }}
+                            />
+                        </>
+                    )}
+                </Stack.Navigator>
+            </NavigationContainer>
         </SafeAreaView>
     );
 }
 
 const styles = StyleSheet.create({
-    container: {
+    appContainer: {
         flex: 1,
-    },
-    bottomBar: {
-        position: 'absolute',
-        left: 0,
-        right: 0,
-        bottom: 0,
     },
 });
 
